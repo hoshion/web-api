965955495abf21d7d1e0e39d6e61b0aff68449fd
just-hoshinon <mwadman2018@gmail.com>
2021-09-01 15:34:59 +0300

    deleting classes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { BTCRateModule } from './btcrate/btcrate.module';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [UserModule, BTCRateModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
